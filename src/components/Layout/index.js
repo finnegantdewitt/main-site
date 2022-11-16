c9d9a482fd34702d836a76f38e02ddc50df7716b
finnegantdewitt <finn@finnegandewitt.com>
2022-11-16 09:11:13 -0800

    weird vscode things

---
+++ src/components/Layout/index.js
@@ -0,0 +1,13 @@
+import Sidebar from "../Sidebar";
+import "./index.scss";
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar />
+      <>HI</>
+    </>
+  );
+};
+
+export default Layout;
