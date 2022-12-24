54ad4dabd94ae0ab4e3b6d8d733a3739f89d91f3
finnegantdewitt <finn@finnegandewitt.com>
2022-12-24 12:42:26 -0800

    side by side tables in comp vis

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,7 +1,4 @@
 import { useEffect, useRef } from "react";
-// import gsap from "gsap-trial";
-// import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
-import LogoS from "../../../assets/images/logo-f1.png";
 import LogoF1SVG from "../../../assets/images/logo-f1.svg";
 import "./index.scss";
 
